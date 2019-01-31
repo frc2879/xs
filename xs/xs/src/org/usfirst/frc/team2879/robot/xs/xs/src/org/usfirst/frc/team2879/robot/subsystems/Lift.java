0136ba35e851ccbd06d5e7adb3c26110c6b5d1f4
Canslp <canslp01@gmail.com>
2019-01-31 15:43:49 -0600

    made some more commands and added the lever subsystem

---
+++ xs/xs/src/org/usfirst/frc/team2879/robot/xs/xs/src/org/usfirst/frc/team2879/robot/subsystems/Lift.java
@@ -0,0 +1,37 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package org.usfirst.frc.team2879.robot.subsystems;
+
+import org.usfirst.frc.team2879.robot.RobotMap;
+
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ * An example subsystem.  You can replace me with your own Subsystem.
+ */
+public class Lift extends Subsystem {
+	// Put methods for controlling this subsystem
+	// here. Call these from Commands.
+	WPI_TalonSRX redline;
+	
+	public Lift(){
+		redline = new WPI_TalonSRX(RobotMap.liftmotor);
+		
+	}
+	
+	public void set(double speed){
+		redline.set(speed);
+	}
+
+	public void initDefaultCommand() {
+		// Set the default command for a subsystem here.
+		// setDefaultCommand(new MySpecialCommand());
+	}
+}
